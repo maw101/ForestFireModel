dc18beb722b95342d44fffdbb0f5e32e5b9f9dda
Matthew Williams (maw101) <maw101@aber.ac.uk>
2019-07-15 12:31:33 +0100

    Create Application.java
    Application class developed to run the fire simulation

---
+++ src/Application.java
@@ -0,0 +1,16 @@
+/**
+ * The type Application.
+ */
+public class Application {
+
+    /**
+     * The entry point of application.
+     *
+     * @param args the input arguments
+     */
+    public static void main(String[] args) {
+        ForestFire fireSimulation = new ForestFire();
+        fireSimulation.runSimulation();
+    }
+
+}
