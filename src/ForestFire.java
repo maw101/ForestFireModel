cd05eeec3bde65ede413670bc18bd6c7284e99b8
Matthew Williams (maw101) <maw101@aber.ac.uk>
2019-07-15 11:39:39 +0100

    Get burning neighbour count

--- src/ForestFire.java
+++ src/ForestFire.java
@@ -2,6 +2,11 @@ public class ForestFire {
 
     private char grid[][];
     private int gridSize;
+    private static final int[][] MOORE_NEIGHBOURS = {
+        {-1, -1},   {-1, 0},    {-1, 1},
+        {0, -1},                {0, 1},
+        {1, -1},    {1, 0},     {1, 1}};
+
 
     public ForestFire() {
         gridSize = 15;
@@ -32,7 +37,25 @@ public class ForestFire {
     }
 
     public void increment() {
-
+        for (int row = 0; row < gridSize; row++) {
+            for (int col = 0; col < gridSize; col++) {
+                
+            }
+        }
     }
+
+    private int countBurningNeighbours(int x, int y) {
+        int count = 0;
+        for (int[] offset : MOORE_NEIGHBOURS)
+            if (isWithinGrid(x + offset[0], y + offset[1]))
+               count++;
+        return count;
+    }
+
+    private boolean isWithinGrid(int x, int y) {
+        return x >= 0 && y >= 0 && x < gridSize && y < gridSize;
+    }
+
+
 
 }
